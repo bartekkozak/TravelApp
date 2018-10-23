fb008694c94e06b47510834db3978ec4c8149459
bartek <bartekkozak3@gmail.com>
2018-10-23 22:22:14 +0200

    added date inputs to flights component

--- src/containers/flights/Flights.js
+++ src/containers/flights/Flights.js
@@ -1,25 +1,99 @@
 import React, { Component } from "react";
 
 class Flights extends Component {
-  state = {};
+  state = {
+    departureDate: "",
+    returnDate: "",
+    origin: "",
+    destination: "",
+    currentDate: "",
+    flights: [],
+    error: ""
+  };
+
+  componentDidMount() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; //January is 0!
+
+    let yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+    today = `${yyyy}-${mm}-${dd}`;
+    this.setState({ currentDate: today });
+  }
 
   getFlights = async e => {
     e.preventDefault();
-    const API_KEY = "8qIKZeA1SAGT1VMnpHcDzuA4FnDdFlN5";
-    const api_call = await fetch(
-      `https://api.sandbox.amadeus.com/v1.2/flights/extensive-search?apikey=${API_KEY}&origin=FRA&destination=LON&departure_date=2018-10-22--2018-10-28`
+    this.setState({ flights: [] });
+    let API_KEY = "8qIKZeA1SAGT1VMnpHcDzuA4FnDdFlN5";
+    let api_call = await fetch(
+      `https://api.sandbox.amadeus.com/v1.2/flights/extensive-search?apikey=${API_KEY}&origin=FRA&destination=LON&departure_date=${
+        this.state.departureDate
+      }--${this.state.returnDate}`
     );
 
-    const data = await api_call.json();
+    let data = await api_call.json();
 
-    console.log(data);
+    if (data.results) {
+      this.setState({ flights: data.results, error: "" });
+    } else {
+      this.setState({ error: "musisz wybrac daty" });
+    }
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
+    const { flights } = this.state;
+    let selectedFlights;
+
+    if (typeof flights !== "undefined" && flights.length > 0) {
+      selectedFlights = flights.map((flight, key) => {
+        return (
+          <li key={key}>
+            CENA LOTU : {flight.price} USD, AIRLINE: {flight.airline},
+            DESTINATION: {flight.destination}, DEPARTURE DATE:
+            {flight.departure_date}, RETURN DATE: {flight.return_date}
+          </li>
+        );
+      });
+    }
+
     return (
       <div>
-        LOTY
-        <button onClick={this.getFlights}>POBIERZ LOTY</button>
+        <form onSubmit={this.getFlights}>
+          <input
+            type="date"
+            name="departureDate"
+            value={this.state.departureDate}
+            onChange={this.onChange}
+            min={this.state.currentDate}
+          />
+          <input
+            type="date"
+            name="returnDate"
+            value={this.state.returnDate}
+            onChange={this.onChange}
+          />
+          <input type="submit" value="pobierz loty" />
+        </form>
+        <p>DEPARTURE DATE: {this.state.departureDate} </p>
+        <p>RETURN DATE: {this.state.returnDate} </p>
+        <p>CURRENT DATE: {this.state.currentDate} </p>
+        <h2>LOTY:</h2>
+        <ul>
+          {typeof this.state.flights !== "undefined" &&
+          this.state.flights.length > 0
+            ? selectedFlights
+            : this.state.error}
+        </ul>
       </div>
     );
   }
--- src/containers/map/CustomMap.js
+++ src/containers/map/CustomMap.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import CustomMarker from "./CustomMarker";
+import Auxiliary from "../../hoc/Auxiliary";
 
 const customIcon = L.icon({
   iconUrl: CustomMarker,
@@ -15,7 +16,38 @@ class CustomMap extends Component {
     latitude: "",
     longitude: "",
     zoom: 2,
-    haveUserLocation: false
+    haveUserLocation: false,
+    cityInput: "",
+    error: ""
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.getGeolocation();
+  };
+
+  getGeolocation = async () => {
+    await fetch(
+      `https://geocode.xyz/Hauptstr.,+57632+${this.state.cityInput}?json=1`
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.error) {
+          this.setState({ error: data.error.description });
+        } else {
+          this.setState({
+            latitude: data.latt,
+            longitude: data.longt,
+            error: ""
+          });
+        }
+      })
+      .catch(err => this.setState({ error: err }));
   };
 
   componentDidMount() {
@@ -47,17 +79,37 @@ class CustomMap extends Component {
     const position = [this.state.latitude, this.state.longitude];
 
     return (
-      <Map className="map__container" center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {!this.state.haveUserLocation ? null : (
-          <Marker position={position} icon={customIcon}>
-            <Popup>TUTAJ BEDZIE JAKIES INFO</Popup>
-          </Marker>
-        )}
-      </Map>
+      <Auxiliary>
+        <Map
+          className="map__container"
+          center={position}
+          zoom={this.state.zoom}
+        >
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {!this.state.haveUserLocation ? null : (
+            <Marker position={position} icon={customIcon}>
+              <Popup>TUTAJ BEDZIE JAKIES INFO</Popup>
+            </Marker>
+          )}
+        </Map>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            placeholder="city"
+            name="cityInput"
+            value={this.state.cityInput}
+            onChange={this.onChange}
+          />
+          <input type="submit" value="szukaj miasta" />
+        </form>
+        <p> {this.state.error} </p>
+        <p>
+          {this.state.latitude} {this.state.longitude}
+        </p>
+      </Auxiliary>
     );
   }
 }
